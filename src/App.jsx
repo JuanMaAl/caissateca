d704cd348f3c4665231020d43e04592775078153
Juan Martínez Alonso <juanmaal1986@gmail.com>
2024-04-13 19:55:10 +0200

    Creación formulario inscripción

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [formData, setFormData] = useState({
+    name: '', email: '', password: ''
+  })
+
+  console.log(formData)
+
+  function handleChange(event){
+    setFormData((prevFormData)=>{
+      return {
+        ...prevFormData,
+        [event.target.name]:event.target.value
+      }
+    })
+  }
+
   return (
-    <div>App</div>
+    <div>
+
+      <h1>Caissateca (Alfa)</h1>
+
+      <form action="">
+        <input 
+        placeholder='Nombre'
+        name='name'
+        onChange={handleChange}
+        />
+        <input 
+        placeholder='Correo electrónico'
+        name='email'
+        onChange={handleChange}
+        />
+        <input 
+        placeholder='Contraseña'
+        name='password'
+        onChange={handleChange}
+        />
+
+        <button type='submite' >
+          Enviar
+        </button>
+
+      </form>
+
+
+
+
+    </div>
   )
 }
 
