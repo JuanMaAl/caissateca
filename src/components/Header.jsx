009933e1fc035cda347b42f206a497be4f63e801
Juan Martínez Alonso <juanmaal1986@gmail.com>
2024-04-19 11:46:14 +0200

    header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,11 +13,14 @@ const Header = ({token}) => {
 
 
   return (
-    <header className="flex justify-end bg-purple-400 w-full pe-10 space-x-3">
-      <h3 className="text-3xl text-white">{token.user.user_metadata.name}</h3> 
-      <BasicButton type={"Button"} text={"Salir"} onClick={handleLogout} 
-      className={"bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"}
-      />
+    <header className="flex justify-between bg-pink-500 w-full px-10 space-x-3 py-2">
+      <h3 className="text-3xl text-white justify-start">Caissateca</h3>
+      <div className="flex justify-between space-x-2">
+        <h3 className="text-3xl text-white">{token.user.user_metadata.name}</h3> 
+        <BasicButton type={"Button"} text={"Salir"} onClick={handleLogout} 
+        className={"bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"}
+        />
+      </div>
     </header>
   )
 }
