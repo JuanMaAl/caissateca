3016a8c32a299b7a765157188f871ff82dd6d517
Juan Martínez Alonso <juanmaal1986@gmail.com>
2024-04-21 04:29:27 +0200

    Estilos, Botones y Varios

--- src/components/tables/BasicTh.jsx
+++ src/components/tables/BasicTh.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BasicTh = (props) => {
   return (
-    <th className="border-4 border-pink-400 p-2 text-pink-800 font-extrabold">{props.text}</th>
+    <th className="border-pink-400 p-2 text-pink-800 font-extrabold">{props.text}</th>
   )
 }
 
