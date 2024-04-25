f0059432110c46d0dd147ef7dec96af29672415c
Juan Martínez Alonso <juanmaal1986@gmail.com>
2024-04-25 07:55:57 +0200

    Componente ViewButton

--- src/components/tables/BasicTh.jsx
+++ src/components/tables/BasicTh.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BasicTh = (props) => {
   return (
-    <td className="p-2 text-pink-800 font-extrabold">{props.text}</td>
+    <td className="p-2 text-pink-800 font-extrabold">{props.text}{props.text2}{props.tex3}</td>
   )
 }
 
---
+++ src/store/bookViewStore.js
@@ -0,0 +1,6 @@
+import create from 'zustand'
+
+export const useBookViewStore= create((set) => ({
+    book: [],
+    bookId: 0
+}))
